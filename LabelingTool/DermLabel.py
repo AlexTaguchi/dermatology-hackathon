f9cc98679e21a850dd87e61d94ec9364a6f05f1f
thatflippingfool <thatflippingfool@gmail.com>
2018-08-28 22:36:53 -0400

    Bug fix - initial photo reliance

--- LabelingTool/DermLabel.py
+++ LabelingTool/DermLabel.py
@@ -71,7 +71,7 @@ def refresh():
         img_new = ImageTk.PhotoImage(img_new)
         image['image'] = img_new
         image.image = img_new
-        image.grid(row=2, column=0, columnspan=10)
+        image.grid(row=3, column=0, columnspan=10)
 
     # Remove buttons when no more images left
     else:
@@ -80,7 +80,7 @@ def refresh():
         submit.destroy()
         file_title['text'] = filename[-40:] + ' (0 remaining)'
         finished = tk.Label(root, text='Done!', fg='black', font='Helvetica 18 bold')
-        finished.grid(row=10, column=0, columnspan=10)
+        finished.grid(row=11, column=0, columnspan=10)
 
 
 def skip():
@@ -91,7 +91,7 @@ def skip():
     image_url = url()
     with open('ratings.txt', 'a') as file:
         file.write('_, _, _, ' + str(filename) + ', ' +
-                   username.get() + ', ' + tag_field.get() +
+                   username_form.get() + ', ' + tag_field.get() +
                    ', ' + image_url + '\n')
     tag_field.delete(0, 'end')
     print(str(filename) + ': no rating')
@@ -101,8 +101,9 @@ def skip():
 
 
 def url():
-   # Identify log file and image number
-    if not os.path.isdir('logs'): os.mkdir('logs')
+    # Identify log file and image number
+    if not os.path.isdir('logs'):
+        os.mkdir('logs')
     log_file = 'logs/' + filename.split('/')[0] + '.txt'
     image_num = int(filename.split('/')[1].split(' ')[0][:-1]) - 1
 
@@ -121,7 +122,7 @@ def submit():
         image_url = url()
         with open('ratings.txt', 'a') as file:
             file.write('%d, %d, %d, ' % tuple(ratings) + str(filename) +
-                       ', ' + username.get() + ', ' + tag_field.get() +
+                       ', ' + username_form.get() + ', ' + tag_field.get() +
                        ', ' + image_url + '\n')
         print(str(filename) + ': %d, %d, %d' % tuple(ratings))
         tag_field.delete(0, 'end')
@@ -133,6 +134,7 @@ def submit():
 def download():
     if url_form.get():
         global file_paths
+        global filename
 
         # Set image download folder
         downloads = os.path.dirname(os.path.abspath(__file__)) + '/images'
@@ -155,9 +157,20 @@ def download():
         # Update remaining number of images to label
         file_title['text'] = file_paths[-1][-40:] + ' (%d remaining)' % len(file_paths)
 
+        # Update filename if necessary
+        if not filename:
+            filename = file_paths.pop()
+            img_new = Image.open('images/' + filename)
+            img_new.thumbnail((w // 2, h // 2))
+            img_new = ImageTk.PhotoImage(img_new)
+            image['image'] = img_new
+            image.image = img_new
+            image.grid(row=3, column=0, columnspan=10)
+
 
 # Get filenames
-if not os.path.isdir('images'): os.mkdir('images')
+if not os.path.isdir('images'):
+    os.mkdir('images')
 file_paths = [y+'/'+x for y in os.listdir('images') if y[0] != '.'
               for x in os.listdir('images/'+y) if x[-4:] == '.jpg']
 
@@ -178,16 +191,22 @@ w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 
 # Title
 title = tk.Label(root, text='Dermatology Image Labeler', fg='black', font='Helvetica 24 bold')
-title.grid(row=0, column=2, columnspan=6)
+title.grid(row=0, column=0, columnspan=10)
 
 # Username
-username = tk.Entry(root)
-username.grid(row=0, column=8, columnspan=2)
+username = tk.Label(root, text='Name:', fg='black', font='Helvetica 18 bold')
+username.grid(row=1, column=1, columnspan=3)
+username_form = tk.Entry(root)
+username_form.grid(row=1, column=3, columnspan=4)
 
 # Filename
-file_title = tk.Label(root, text=str(file_paths[-1:][-40:]) + ' (%d remaining)' % len(file_paths),
-                      fg='black', font='Helvetica 18')
-file_title.grid(row=1, column=0, columnspan=10)
+if not file_paths:
+    file_title = tk.Label(root, text='(%d remaining)' % len(file_paths),
+                          fg='black', font='Helvetica 18')
+else:
+    file_title = tk.Label(root, text=file_paths[-1][-40:] + ' (%d remaining)' % len(file_paths),
+                          fg='black', font='Helvetica 18')
+file_title.grid(row=2, column=0, columnspan=10)
 
 # Image
 if file_paths:
@@ -196,69 +215,69 @@ if file_paths:
     img.thumbnail((w//2, h//2))
     img = ImageTk.PhotoImage(img)
     image = tk.Label(root, height=h//2, width=h//2, image=img)
-    image.grid(row=2, column=0, columnspan=10)
+    image.grid(row=3, column=0, columnspan=10)
 else:
     filename = ''
-    img = Image.open('cat.jpg')
+    img = Image.new('RGB', (1, 1), 'white')
     img.thumbnail((w//2, h//2))
     img = ImageTk.PhotoImage(img)
     image = tk.Label(root, height=h//2, width=h//2, image=img)
-    image.grid(row=2, column=0, columnspan=10)
+    image.grid(row=3, column=0, columnspan=10)
 
 # Redness
 red_title = tk.Label(root, text='Redness', fg='black', font='Helvetica 18')
-red_title.grid(row=3, column=0, columnspan=10)
+red_title.grid(row=4, column=0, columnspan=10)
 red_button = []
 for i in range(10):
-    red_button += [tk.Button(root, text=str(i), width=h//250, fg='black', font='Arial 18',
+    red_button += [tk.Button(root, text=str(i), width=h//250, fg='black', font='Arial 20',
                              command=lambda rating=i: redness(rating))]
-    red_button[i].grid(row=4, column=i)
+    red_button[i].grid(row=5, column=i)
 
 # Texture
 texture_title = tk.Label(root, text='Texture', fg='black', font='Helvetica 18')
-texture_title.grid(row=5, column=0, columnspan=10)
+texture_title.grid(row=6, column=0, columnspan=10)
 texture_button = []
 for i in range(10):
-    texture_button += [tk.Button(root, text=str(i), width=h//250, fg='black', font='Arial 18',
+    texture_button += [tk.Button(root, text=str(i), width=h//250, fg='black', font='Arial 20',
                                  command=lambda rating=i: texture(rating))]
-    texture_button[i].grid(row=6, column=i)
+    texture_button[i].grid(row=7, column=i)
 
 # Evenness
 even_title = tk.Label(root, text='Evenness', fg='black', font='Helvetica 18')
-even_title.grid(row=7, column=0, columnspan=10)
+even_title.grid(row=8, column=0, columnspan=10)
 even_button = []
 for i in range(10):
-    even_button += [tk.Button(root, text=str(i), width=h//250, fg='black', font='Arial 18',
+    even_button += [tk.Button(root, text=str(i), width=h//250, fg='black', font='Arial 20',
                               command=lambda rating=i: evenness(rating))]
-    even_button[i].grid(row=8, column=i)
+    even_button[i].grid(row=9, column=i)
 
 # Gap
 gap = tk.Label(root, bg='white')
-gap.grid(row=9, column=0, columnspan=10)
+gap.grid(row=10, column=0, columnspan=10)
 
 # Tag
-tag_title = tk.Label(root, text='Tag', fg='black', font='Helvetica 18')
-tag_title.grid(row=10, column=1, columnspan=4)
+tag_title = tk.Label(root, text='Tag', fg='black', font='Helvetica 16 bold')
+tag_title.grid(row=11, column=1, columnspan=4)
 
 # Download button
-url_button = tk.Button(root, text='Download', font='Helvetica 18 bold', command=download)
-url_button.grid(row=10, column=5, columnspan=4)
+url_button = tk.Button(root, text='Download', font='Helvetica 16 bold', command=download)
+url_button.grid(row=11, column=5, columnspan=4)
 
 # Skip button
-skip = tk.Button(root, text='Skip', font='Helvetica 18 bold', command=skip)
-skip.grid(row=11, column=0, columnspan=1)
+skip = tk.Button(root, text='Skip', font='Helvetica 16 bold', command=skip)
+skip.grid(row=12, column=0, columnspan=1)
 
 # Submit button
-submit = tk.Button(root, text='Submit', font='Helvetica 18 bold', command=submit)
-submit.grid(row=11, column=9, columnspan=1)
+submit = tk.Button(root, text='Submit', font='Helvetica 16 bold', command=submit)
+submit.grid(row=12, column=9, columnspan=1)
 
 # Tag form field
 tag_field = tk.Entry(root)
-tag_field.grid(row=11, column=1, columnspan=4)
+tag_field.grid(row=12, column=1, columnspan=4)
 
 # Url form field
 url_form = tk.Entry(root)
-url_form.grid(row=11, column=5, columnspan=4)
+url_form.grid(row=12, column=5, columnspan=4)
 
 # Make GUI interface appear
 root.mainloop()
